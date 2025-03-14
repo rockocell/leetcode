51c5f9a2003609e6d8f0eeedd7661b84aa1e5f4c
MJ Kim <gwang907@gmail.com>
2025-03-14 09:38:56 +0900

    Time: 69 ms (23.18%), Space: 159.5 MB (5.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.dart
+++ 0283-move-zeroes/0283-move-zeroes.dart
@@ -1,14 +1,14 @@
 class Solution {
   void moveZeroes(List<int> nums) {
-    for(int n = 0 ; n <= nums.length-1 ; n ++) {
-        for(int i = n; i <= nums.length-1 ; i++) {
+    for(int n = 0 ; n < nums.length ; n ++) {
+        for(int i = n; i < nums.length ; i++) {
             if(i != 0 && nums[i]!=0) {
                 if(nums[n]==0) {
                     nums[n]=nums[i];
                     nums[i]=0;
                     break;
-                } else {}
-            } else {}
+                }
+            }
         }
     }
   }
