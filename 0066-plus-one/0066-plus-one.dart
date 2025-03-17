720d5159e6e8502d9b271bf86c965ff98cf6f568
MJ Kim <gwang907@gmail.com>
2025-03-17 09:41:12 +0900

    Time: 1 ms (27.75%), Space: 152 MB (23.35%) - LeetHub

--- 0066-plus-one/0066-plus-one.dart
+++ 0066-plus-one/0066-plus-one.dart
@@ -1,20 +1,19 @@
 class Solution {
-  List<int> plusOne(List<int> digits) {int n = digits.length;
+  List<int> plusOne(List<int> digits) {
+    int n = digits.length;
 
-  for (var i = n - 1; i > -1; i--) {
-    if (digits[i] == 9) {
-      digits[i] = 0;
-    } else {
-      digits[i]++;
-      break;
+    for (int i = n-1; i > -1; i--) {
+        if (digits[i] == 9) {
+            digits[i] = 0;
+            } else {
+            digits[i]++;
+            break;
+        }
     }
-  }
-  
-  var check = digits.where((c) => c==0);
-  if(check.length==n) {
-    digits.insert(0,1);
-  }
-
+    var check = digits.where((c)=> c==0);
+    if(check.length == n) {
+        digits.insert(0,1);
+    }
     return digits;
   }
 }
