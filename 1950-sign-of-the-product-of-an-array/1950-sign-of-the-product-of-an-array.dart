3c1d7d7ddccb8a5fa3b9aa73e9bc33df760fa589
MJ Kim <gwang907@gmail.com>
2025-03-19 09:12:52 +0900

    Time: 2 ms (0%), Space: 149 MB (50%) - LeetHub

--- 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.dart
+++ 1950-sign-of-the-product-of-an-array/1950-sign-of-the-product-of-an-array.dart
@@ -1,14 +1,12 @@
 class Solution {
   int arraySign(List<int> nums) {
     //any로 0 있는지 확인
-    var checkZero = nums.any((n) => n == 0);
-    if (checkZero == true) {
+    if (nums.any((n) => n == 0) == true) {
         return 0;
     } else {
         //where로 모든 음수 구하기
         //length로 음수 개수 구하기
-        var oddLength = (nums.where((n) => n<0)).length;
-        if(oddLength.isOdd==true) {
+        if(((nums.where((n) => n<0)).length).isOdd==true) {
             return -1;
         } else {
             return 1;
