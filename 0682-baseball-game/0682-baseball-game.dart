64e0e6af5702884a8c2dfcd6974963f485b1086e
MJ Kim <gwang907@gmail.com>
2025-03-20 09:46:00 +0900

    Time: 7 ms (11.76%), Space: 146.8 MB (91.18%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.dart
+++ 0682-baseball-game/0682-baseball-game.dart
@@ -13,14 +13,10 @@ class Solution {
         } else {
             switch(operations[i]) {
                 case 'C':
-                    if (score.isNotEmpty) {
                     score.removeLast();
-                    }
                     break;
                 case 'D':
-                    if (score.isNotEmpty) {
                     score.add(score[score.length-1]*2);
-                    }
                     break;
                 case '+':
                     if (score.length>=2) {
