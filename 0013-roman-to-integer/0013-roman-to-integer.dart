add151e41bb8ec183758e2a48f79869a46ea5e1c
MJ Kim <gwang907@gmail.com>
2025-03-24 09:16:13 +0900

    Time: 7 ms (80.87%), Space: 149.2 MB (73.12%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.dart
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -1,50 +1,48 @@
 class Solution {
   int romanToInt(String s) {
-    //String s를 리스트화
-    //문자가 나타내는 수를 담은 리스트 생성
-    //for 문으로 리스트의 모든 수 더해주기
-    //list[i]의 값보다 list[i-1]의 값이 작은 경우 list[i]값을 더한 뒤 list[i-1]*2 빼기
+    //s를 리스트화
+    //for문 - 리스트 내의 요소를 각각 정수로 치환, 하나씩 최종값result에 더해줌
+    //list[i-1]이 list[i]보다 작은 경우 -list[i-1]*2
 
-    List<String> sList = s.split('');
-    List<int> iList = [];
-    int result = 0;
-    
-    for (int i = 0; i < sList.length; i++) {
-        switch(sList[i]) {
-            case 'I' :
-            iList.add(1);
+    List list = s.split('');
+    List<int> intList = [];
+
+    for (var letter in list) {
+        switch(letter) {
+            case 'I':
+            intList.add(1);
             break;
-            case 'V' :
-            iList.add(5);
+            case 'V':
+            intList.add(5);
             break;
-            case 'X' :
-            iList.add(10);
+            case 'X':
+            intList.add(10);
             break;
-            case 'L' :
-            iList.add(50);
+            case 'L':
+            intList.add(50);
             break;
-            case 'C' :
-            iList.add(100);
+            case 'C':
+            intList.add(100);
             break;
-            case 'D' :
-            iList.add(500);
+            case 'D':
+            intList.add(500);
             break;
-            case 'M' :
-            iList.add(1000);
+            case 'M':
+            intList.add(1000);
             break;
         }
     }
-    for (int i =0; i<iList.length; i++) {
-        if (i != 0) {
-            if (iList[i-1]<iList[i]) {
-                result = result + iList[i] - iList[i-1]*2;
-            } else {
-                result += iList[i];
-            }
+
+    int result = intList[0];
+
+    for (int i = 1; i < intList.length ; i++) {
+        if (intList[i-1]<intList[i]) {
+            result = result + intList[i] - intList[i-1]*2;
         } else {
-            result += iList[i];
+            result += intList[i];
         }
     }
+
     return result;
   }
 }
