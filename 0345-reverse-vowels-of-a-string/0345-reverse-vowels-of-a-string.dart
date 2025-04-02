71b1634b0f306755d0ccceaaca166ec5f8d0d747
MJ Kim <gwang907@gmail.com>
2025-04-02 09:30:18 +0900

    Time: 484 ms (13.33%), Space: 158.9 MB (6.67%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.dart
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.dart
@@ -3,17 +3,21 @@ class Solution {
     //반복문으로 s의 각 문자를 확인 => 모음인 경우만 List<String> list 에 넣기
     //반복문으로 s의 모음마다 list의 값 뒤에서부터 가져와서 바꾸기 
 
+
+
     List<String> list = [];
     String result = '';
     int count = 0;
 
+    List<String> vower = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+
     for(int i = 0 ; i < s.length ; i++) {
-        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') {
+        if (vower.contains(s[i])) {
             list.add(s[i]);
         }
     }
     for(int i = 0 ; i < s.length ; i++) {
-        if (s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') {
+        if (vower.contains(s[i])) {
             result = result + list[list.length-1-count];
             count++;
         } else {
