d754a23198dc14b9c89df75f417849622b03cf61
MJ Kim <gwang907@gmail.com>
2025-04-10 09:16:06 +0900

    Time: 38 ms (27.59%), Space: 153.3 MB (27.59%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.dart
@@ -0,0 +1,26 @@
+class Solution {
+  List<List<int>> findDifference(List<int> nums1, List<int> nums2) {
+    // nums1, nums2 내부의 값부터 처리 < 중복되는 요소 삭제
+    // nums1의 모든 요소-> contains로 nums2에도 있는지 확인, 없는 것 answer[0] 에 넣기
+    // nums2 요소 중 answer[0]에 없는 것 answer[1]에 넣기
+    Set<int> set1 = Set<int>.from(nums1);
+    Set<int> set2 = Set<int>.from(nums2);
+
+    List<List<int>> answer = [[],[]];
+    List<int> common = [];
+
+    for(int s in set1) {
+        if(!set2.contains(s)) {
+            answer[0].add(s);
+        } else {
+            common.add(s);
+        }
+    }
+    for(int s in set2) {
+        if(!common.contains(s)) {
+            answer[1].add(s);
+        } else continue;
+    }
+    return answer;
+  }
+}
