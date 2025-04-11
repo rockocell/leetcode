a809e874d79e4bb5b143f0e54a5f0aa61f76ef43
MJ Kim <gwang907@gmail.com>
2025-04-11 09:47:15 +0900

    Time: 5 ms (86.36%), Space: 147.6 MB (95.45%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.dart
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.dart
@@ -4,14 +4,13 @@ class Solution {
     // List<List<int>> list = [[],[]]
     // list[0]에는 요소, list[1]에는 해당 요소가 몇 개 있는지 넣기
     // arr를 for문으로 돌기
-    // 요소 하나하나를 확인: 이미 list[0]에 있는 값이면 해당값 인덱스를 찾아 list[1][index] ++; , 없으면 list[0]에 값 추가, list[1].add(1);
+    // 요소 하나하나를 확인: 이미 list[0]에 있는 값이면 해당값 인덱스를 찾아 list[1][index] ++; , 없으면 list[0].add(a); list[1].add(1);
 
     List<List<int>> list = [[],[]];
 
     for(int a in arr){
         if (list[0].contains(a)){
-            int index = list[0].indexOf(a);
-            list[1][index] ++;
+            list[1][list[0].indexOf(a)] ++;
         } else {
             list[0].add(a);
             list[1].add(1);
