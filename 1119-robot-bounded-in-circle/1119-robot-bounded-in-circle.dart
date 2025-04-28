533d943950aae2b3c7bb55fd2fd597133a37ccbb
MJ Kim <gwang907@gmail.com>
2025-04-28 09:32:57 +0900

    Time: 1 ms (100%), Space: 148.3 MB (0%) - LeetHub

--- 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.dart
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.dart
@@ -1,15 +1,13 @@
 class Solution {
   bool isRobotBounded(String instructions) {
-   // 최종 각도가 0도, 360도 ... 일때 => 반복할때마다 제자리에 있으면 true 
-   // 최종 각도가 90도, 180도, 270도, ... 일 때 => 항상 true
-
-   // 각도 구하기 => L의 개수 - R의 개수 = n * -90 이 최종 각도
+    // 최종 각도가 0도, 360도 ... 일때 => 반복할때마다 제자리에 있으면 true 
+    // 최종 각도가 90도, 180도, 270도, ... 일 때 => 항상 true
 
     int angle = 0;
     int x = 0;
     int y = 0;
 
-   for(String i in instructions.split('')){
+    for(String i in instructions.split('')){
         if(i == 'L'){
             angle --;
         }
@@ -22,12 +20,12 @@ class Solution {
             if ((angle.abs())%4 == 2) {x--;}
             if ((angle.abs())%4 == 3) {y++;}
         }
-   }
+    }
    
-   if((angle.abs())%4 != 0){return true;}
-   else if(x==0 && y==0){
+    if((angle.abs())%4 != 0){return true;}
+    else if(x==0 && y==0){
     return true;
-   }
+    }
     return false;
-  }
+    }
 }
