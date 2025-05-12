3bc99ea0b4a29bbd0e0c4a930180da6a6f0a9d04
MJ Kim <gwang907@gmail.com>
2025-05-12 09:18:17 +0900

    Time: 0 ms (100%), Space: 153.9 MB (7.69%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.dart
@@ -0,0 +1,30 @@
+class Solution {
+  int diagonalSum(List<List<int>> mat) {
+    // mat[0]에서는 0, last
+    // mat[1]에서는 1, last-1
+    // ...
+    // mat.length/2 의 몫만큼 양쪽이 반복, mat.length.isOdd==true인경우 가운데 행 하나 있음
+
+    // mat.length가 홀수인 경우
+    // for 문으로 돌기
+    // mat[0][0]+mat[0][last]
+    // mat[1][1]+mat[a][last-1]
+    
+    int result = 0;
+
+    if(mat.length.isOdd){
+        for(int i = 0; i < mat.length; i++){
+            if(i != (mat.length-1-i)) {
+                result += mat[i][i] + mat[i][(mat.length-1-i)];
+            } else {
+                result += mat[i][i];
+            }
+        }
+    } else {
+        for(int i = 0; i < mat.length; i++){
+            result += mat[i][i] + mat[i][(mat.length-1-i)];
+        }
+    }
+    return result;
+  }
+}
